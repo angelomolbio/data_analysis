9a4d11da9f01a7b991b6a413de5edafae13dd315
Angelo Scarciglia <angeloscarciglia@MacBook-di-Angelo.local>
2022-06-03 00:51:11 +0200

    esercizi passo passo

--- lessons.R
+++ lessons.R
@@ -5,3 +5,10 @@ downloadContainers(group="docker")
 install_github("kendomaniac/rCASC", ref="master")
 library(rCASC)
 downloadContainers(group="docker")
+
+#Fastqcsetting 
+#http://users.path.ox.ac.uk/~pcook/w1/NGS_workshop2.htm
+#sudo ln -s /home/angeloscarciglia/Desktop/calogero/FastQC/fastqc /usr/local/bin/fastqc
+#chmod + fastqc in FastQC folder downloaded
+#./fastqc
+# in cd dataset0 cartella del file -> fastqc ra100k.R1.fastq
