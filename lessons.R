eed21159401a45dae5f3cf71826bb42242d96d23
Angelo Scarciglia <angeloscarciglia@MacBook-di-Angelo.local>
2022-06-03 13:43:45 +0200

    continue to upload info

--- lessons.R
+++ lessons.R
@@ -17,3 +17,35 @@ downloadContainers(group="docker")
 #cd folder containing the file 
 #docker run -t -v `pwd`:`pwd` -w `pwd` ewels/multiqc
 
+#create image of ubuntu with specific requisiti
+#docker pull ubuntu:21.04
+#docker run -i -t ubuntu:21.04
+#apt-get update/ upgrade/ apt-get install sudo / apt-get install nano
+#once done exit and docker commit "istance in which we have done what we need" ubuntu:21.04.angelo (this final point provided the tag included)
+
+#create an R file with the command install.packages('umap', repos='http://cran.us.r-project.org')#saved as umap_command.R in the same folder of Dockerfile
+#create the dockerfile and the container from it 
+#mkdir DockerFile (generate the folder)
+#touch Dockerfile (generate the file with no extension)
+#vim Dockerfile (go in the file)
+#FROM rocker/r-ubuntu:20.04	
+#RUN apt-get -y update
+#RUN apt-get -y upgrade
+#RUN apt-get -y install sudo 
+#RUN apt-get -y install nano
+#COPY ./umap_command.R/ home/
+#RUN chmod +x /home/umap_command.R
+#RUN sudo apt-get update && sudo apt-get -y install libssl-dev
+
+#i to write and esc :wq! to exit saving
+###After creating the Dockerfile goes on the terminal and type the command below.
+#docker build Dockerfile -t r4:v.0.01 #build the docker 
+#docker run -i -t r4:v.0.01 #run the docker
+#sudo ls #check that sudo is present and work
+#Then go in the home dir
+#Rscript ./umap_command.R #run the R file to install umap exit
+#docker ps -a #find the instance name ID of the instance previously closed 
+#docker commit instaceID (find to be in my case d7a30dac5e11 through docker ps -a) r4:v.0.01 #commit the changes to the docker
+#docker run -i -t r4:v.0.01 #run again the docker
+#sudo ls #check that the changes were saved
+#exit
